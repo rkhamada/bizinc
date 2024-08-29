356547c60525ba36613dd8955880ea61fbf09866
Ricardo Hamada <ricardo.hke@gmail.com>
2024-08-28 23:26:42 -0300

    Initial commit

---
+++ bizinc_backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3333;
+
+app.get('/message', (req, res) => {
+  res.json({ message: "Hello, World!" });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
